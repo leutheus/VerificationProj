fc7accd2e3d10500a8b36e42a9bc53de9bcf5dd3
Superschnitzel <leutheusser@googlemail.com>
2013-11-18 14:36:53 +0100

    testsomstuff

--- database/admin.py
+++ database/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from database.models import Run, Configuration, Benchmark, File
+
+admin.site.register(Run)
+admin.site.register(Configuration)
+admin.site.register(Benchmark)
+admin.site.register(File)
--- database/models.py
+++ database/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Benchmark(models.Model):
+	user = models.CharField(max_length=255)
+	def __unicode__(self):
+		return self.user
+
+
+class Run(models.Model):
+	revision = models.IntegerField(default=0)
+
+class File(models.Model):
+	inBenchmark = models.ForeignKey(Benchmark)
+	filename = models.CharField(max_length=255)
+	filesize = models.IntegerField(default=0)
+	date_added = models.DateTimeField('date added')
+	date_tested = models.DateTimeField('date of last test')
+	expected_result = models.CharField(max_length=255)
+	tested_result = models.CharField(max_length=255)
+	binary = models.FileField(db_index = True, upload_to ='testfiles')
+
+class Configuration(models.Model):
+	timelimit = models.IntegerField(default=0)
