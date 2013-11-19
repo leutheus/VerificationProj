c455911caa67455eed868070339af32b58262816
Tom Leutheus <leutheusser@googlemail.com>
2013-11-19 18:27:17 +0100

    settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'database'
+    'database',
+    'gunicorn',
 )
 
 MIDDLEWARE_CLASSES = (
