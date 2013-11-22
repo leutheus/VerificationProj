3c295a5db49330657a16c046e5a7d3ef5897dd6c
Tom Leutheus <leutheusser@googlemail.com>
2013-11-22 11:38:49 +0100

    styleshsets

--- myproject/settings.py
+++ myproject/settings.py
@@ -102,7 +102,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = 'srv/www/database/static/'
+STATIC_URL = ''
 
 STATICFILES_DIRS = (
     #Sos.path.join(BASE_DIR, '..', '/static/'),
