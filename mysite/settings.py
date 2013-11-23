c756e259a6b4332fa6f0c58eaf4cdb924bdb6e68
Superschnitzel <leutheusser@googlemail.com>
2013-11-23 12:14:36 +0100

    test

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,7 +76,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/home/leutheus/cloudcontrolDjango/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -88,6 +88,11 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
 )
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, "static"),
+    '/var/www/static/',
+)
+print os.path.join(PROJECT_ROOT, "static")
 
 # List of finder classes that know how to find static files in
 # various locations.
